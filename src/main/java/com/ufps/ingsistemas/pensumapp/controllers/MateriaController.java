471e4480adfa1b1f29ae3148fa0075b404dbbd12
CristianCamargo04 <cristianfernandocc@ufps.edu.co>
2022-06-24 22:35:21 -0500

    implementacion de cors

--- src/main/java/com/ufps/ingsistemas/pensumapp/controllers/MateriaController.java
+++ src/main/java/com/ufps/ingsistemas/pensumapp/controllers/MateriaController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/materia")
+@CrossOrigin
 public class MateriaController {
     @Autowired
     MateriaService materiaService;
--- src/main/java/com/ufps/ingsistemas/pensumapp/controllers/SemestreController.java
+++ src/main/java/com/ufps/ingsistemas/pensumapp/controllers/SemestreController.java
@@ -4,15 +4,16 @@ import com.ufps.ingsistemas.pensumapp.models.output.SemestreOutput;
 import com.ufps.ingsistemas.pensumapp.services.SemestreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/semestre")
+@CrossOrigin
 public class SemestreController {
     @Autowired
     SemestreService semestreService;
