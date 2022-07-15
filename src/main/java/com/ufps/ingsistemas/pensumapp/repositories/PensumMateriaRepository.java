e22c43bde77d3b59a38dbdd8da177f6c115c3606
CristianCamargo04 <cristianfernandocc@ufps.edu.co>
2022-07-14 21:55:55 -0500

    fix lista de creditos y codios de prerequisitos en VO

--- src/main/java/com/ufps/ingsistemas/pensumapp/repositories/PensumMateriaRepository.java
+++ src/main/java/com/ufps/ingsistemas/pensumapp/repositories/PensumMateriaRepository.java
@@ -13,17 +13,17 @@ public interface PensumMateriaRepository extends CrudRepository<PensumMateriaEnt
 
     @Query(value = "SELECT pm.id as idMateriaPensum, pm.cod_pensum as codPensum, " +
             "m.id as idMateria, pm.codigo, m.nombre, m.horas, m.creditos, pm.semestre, pm.electiva, " +
-            "pm.cod_perrequisito, pm.cre_perrequisito FROM pensum_materia pm INNER JOIN materias m " +
+            "pm.cod_perrequisito as codPerrequisito, pm.cre_perrequisito as crePerrequisito FROM pensum_materia pm INNER JOIN materias m " +
             "ON pm.id_materia = m.id", nativeQuery = true)
     List<MateriaPensumVO> findAllMaterias();
 
     @Query(value = "SELECT pm.id as idMateriaPensum, pm.cod_pensum as codPensum, " +
             "m.id as idMateria, pm.codigo, m.nombre, m.horas, m.creditos, pm.semestre, pm.electiva, " +
-            "pm.cod_perrequisito, pm.cre_perrequisito FROM pensum_materia pm INNER JOIN materias m " +
+            "pm.cod_perrequisito as codPerrequisito, pm.cre_perrequisito as crePerrequisito FROM pensum_materia pm INNER JOIN materias m " +
             "ON pm.id_materia = m.id WHERE pm.cod_pensum = :codPensum and pm.semestre < :semestre", nativeQuery = true)
     List<MateriaPensumVO> findAllMateriasPrerrequisitos(String codPensum, Integer semestre);
 
-    @Query(value = "SELECT pm.id, m.id as idMateria, pm.codigo, m.nombre, m.horas, m.creditos, pm.semestre, pm.cod_perrequisito, pm.cre_perrequisito " +
+    @Query(value = "SELECT pm.id, m.id as idMateria, pm.codigo, m.nombre, m.horas, m.creditos, pm.semestre, pm.cod_perrequisito as codPerrequisito, pm.cre_perrequisito as crePerrequisito" +
             "    FROM pensum_materia pm INNER JOIN materias m  ON pm.id_materia = m.id " +
             "    WHERE pm.cod_pensum = :codPensum AND pm.semestre = :semestre AND pm.electiva= 0 " +
             "    UNION " +
@@ -32,7 +32,7 @@ public interface PensumMateriaRepository extends CrudRepository<PensumMateriaEnt
             "    WHERE pe.cod_pensum = :codPensum AND pe.semestre = :semestre", nativeQuery = true)
     List<MateriaMallaVo> findAllMateriasByPensumBySemestre(String codPensum, String semestre);
 
-    @Query(value = "SELECT pm.id, m.id as idMateria, pm.codigo, m.nombre, m.horas, m.creditos, pm.semestre, pm.cod_perrequisito, pm.cre_perrequisito " +
+    @Query(value = "SELECT pm.id, m.id as idMateria, pm.codigo, m.nombre, m.horas, m.creditos, pm.semestre, pm.cod_perrequisito as codPerrequisito, pm.cre_perrequisito as crePerrequisito " +
             "FROM pensum_materia pm INNER JOIN materias m  ON pm.id_materia = m.id " +
             "WHERE pm.cod_pensum = :codPensum AND pm.electiva = 1 AND m.creditos = :creditos", nativeQuery = true)
     List<ElectivaMallaVO> findAllElectivasByPensumByCreditos(String codPensum, String creditos);
